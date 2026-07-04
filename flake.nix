7e82d6bc6e5539ab4c70add05d94b548bfc85986
Tristan Partin <tristan@partin.io>
2026-07-04 14:48:24 -0500

    Refactor ghostty input to include nixpkgs dependency
    This allows downstreams to override the nixpkgs input.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    ghostty.url = "github:ghostty-org/ghostty";
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+      inputs.nixpkgs = nixpkgs;
+    }
   };
 
   outputs = { self, nixpkgs, ghostty }:
